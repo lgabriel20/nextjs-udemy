0b1cbb823bebd73603539c4a2db7c60d9a2c6fbd
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-05-19 12:08:12 -0500

    lifting state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,25 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from './PostsList.module.css'
+import { useState } from "react";
 
 function PostsList(){
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event){
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event){
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
       <ul className={classes.posts}>
-        <Post author="Lianne" body="React.js is Awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Sandy" body="Check out the full course!" />
       </ul>
     </>
